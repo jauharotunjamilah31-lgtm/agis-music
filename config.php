a3b6b46cd13062170c432ef66e34c0099a466adb
jauharotunjamilah31-lgtm <jauharotunjamilah31@guru.sma.belajar.id>
2025-11-20 10:20:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Ganti dengan username MySQL Anda
+$password = ""; // Ganti dengan password MySQL Anda
+$dbname = "mipa_lab";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
